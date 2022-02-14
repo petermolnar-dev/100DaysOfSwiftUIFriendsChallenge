0a70552d1a7490e44c4d49d6e44ea65d4a53277d
Peter Molnar <petermolnar.hu@gmail.com>
2022-02-14 21:51:25 +0100

    Initial Commit

---
+++ FriendsChallenge/FriendsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsChallengeApp.swift
+//  FriendsChallenge
+//
+//  Created by Peter Molnar on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
